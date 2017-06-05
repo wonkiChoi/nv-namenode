d756cfad94f8e8b3445f40340644acc48fe7bce9
WonGi Choi <91cwk@naver.com>
2017-06-05 15:08:42 +0900

    initial commit

---
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/nvram_mmap.java
@@ -0,0 +1,34 @@
+package org.apache.hadoop.hdfs.server.namenode;
+
+import java.nio.ByteBuffer;
+
+import org.apache.hadoop.io.nativeio.NativeIO;
+import org.apache.hadoop.io.nativeio.NativeIOException;
+import org.apache.hadoop.util.NativeCodeLoader;
+import org.apache.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class nvram_mmap {
+	//  public static final java.util.logging.Logger LOG =
+		//      LoggerFactory.getLogger(nvram_mmap.class.getName());	
+	void nvram_mmap_test (int size) {
+		try {
+			if(NativeCodeLoader.isNativeCodeLoaded()){
+			ByteBuffer byteBuffer = ByteBuffer.allocateDirect(100);
+			//LOG.info(byteBuffer);
+			//LOG.info("TEST start");
+			System.out.println(byteBuffer);
+			NativeIO.getMemlockLimit0();
+			NativeIO.allocateNVRAMBuffer(size);
+			System.out.println(byteBuffer);
+			}
+			//LOG.info(byteBuffer);
+		} catch (NativeIOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
