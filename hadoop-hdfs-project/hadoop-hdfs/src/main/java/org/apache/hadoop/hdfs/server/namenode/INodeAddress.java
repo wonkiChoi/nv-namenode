8875660e39301268017ed9d06468aa12cb499995
WonGi Choi <91cwk@naver.com>
2018-04-03 15:31:21 +0900

    Nvrammpa modification

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeAddress.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeAddress.java
@@ -1,5 +1,64 @@
 package org.apache.hadoop.hdfs.server.namenode;
 
 public class INodeAddress {
+	private int location;
+	private long parent_id;
+	private long id;
+	private int child_num;
+	private int last_page;
+	public INodeAddress () {
+		this.location = 0;
+		this.parent_id = 0;
+		this.id = 0;
+		this.child_num = 0;
+		this.last_page = 0;
+	}
+	public INodeAddress (int location, long parent_id , long id, int child_num, int last_page ) {
+		this.location = location;
+		this.parent_id = parent_id;
+		this.id = id;
+		this.child_num = child_num;
+		this.last_page = last_page;
+	}
+	
+	public int getLocation() {
+		return location;
+	}
+	
+	public long getParentId() {
+		return parent_id;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public int getNumChild() {
+		return child_num;
+	}
+	
+	public int getLastPage() {
+		return last_page;
+	}
+	
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
+	public void setParentId(long parent_id) {
+		 this.parent_id = parent_id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public void setNumChild(int child_num) {
+		this.child_num = child_num;
+	}
+	
+	public void setLastPage(int last_page) {
+		this.last_page = last_page;
+	}
 
 }
